fc7e30b4903a3344c60e0b6a706ffc9e61e1336c
a.m.student <asumehrocks@gmail.com>
2021-01-27 08:46:17 -0600

    X and O up

--- TicTacToeAM/ContentView.swift
+++ TicTacToeAM/ContentView.swift
@@ -19,6 +19,12 @@ struct ContentView: View {
 }
 
 struct Home : View {
+    
+    
+    @State var moves : [String] = Array(repeating: "", count: 9)
+    
+    @State var isPlaying = true
+    
     var body: some View {
         VStack {
             
@@ -26,16 +32,51 @@ struct Home : View {
                 
                 ForEach(0..<9, id: \.self) { index in
                     
-                    Color.white
+                    ZStack {
+                        Color.white
+                        
+                        Text(moves[index])
+                            .font(.system(size: 55))
+                            .fontWeight(.heavy)
+                            .foregroundColor(.black)
+                        
+                    }
+                    
+                        .frame(width: getWidth(), height: getWidth())
+                        .cornerRadius(15)
+                    .onTapGesture (perform: {
+                        withAnimation(Animation.easeIn(duration: 0.5)) {
+                            
+                            
+                            moves[index] = isPlaying ? "X" : "O"
+                            isPlaying.toggle()
+                        
+                        }
+                    })
+                
                 }
                 
             }
+            
+            .padding(15)
+            
         }
+        
     }
-}
+    
+    
+    
+    func getWidth() -> CGFloat {
+        let width = UIScreen.main.bounds.width - (30 + 30)
+        
+        return width / 3
+    }
 
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        }
     }
+    
 }
